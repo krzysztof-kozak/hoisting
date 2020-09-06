306dee37a80fce87ffb9b7802ec3f4e6cc11577a
Kris <hithard5@gmail.com>
2020-09-06 18:29:02 +0000

    Add function declaration vs expression comparison

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// hoisting
+// hoisting var vs let/const
 
 function hoist(){
   console.log(foo); // foo was moved up to the top of its scope but it was not assigned any value yet, so the default unefined value remains!
@@ -10,7 +10,21 @@ function hoist(){
 
 }
 
-hoist();
-
+// hoist();
 // var = hoisted, and declared, and initialized, but not assigned!
 // const/let = hoisted, and declared, but NOT initialized and not assigned!
+
+
+// hoisting function declaration vs function expression
+
+hoistFnDeclaration(); // The entire function is hoisted and we get the result we want:)
+
+function hoistFnDeclaration() {
+  console.log("Hoist me up, Scotty!")
+}
+
+hoistFnExpression(); // Only the variable is hoisted, and not the entire function. As we learned in the previous examples, variables declared with const/let are hoisted but are not initialized so we get an error cannot access ... before initialization.
+
+const hoistFnExpression = function() {
+  console.log("Hoist me up, Scotty!")
+}
